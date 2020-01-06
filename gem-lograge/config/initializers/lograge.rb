24a5dad34443d911f6f5265430e0c11cb32e3250
thaim <thaim24@gmail.com>
2020-01-06 20:16:10 +0900

    add request parameter as logging

--- gem-lograge/config/initializers/lograge.rb
+++ gem-lograge/config/initializers/lograge.rb
@@ -6,6 +6,9 @@ Rails.application.configure do
   config.lograge.logger = ActiveSupport::Logger.new "#{Rails.root}/log/lograge_#{Rails.env}.log"
 
   config.lograge.custom_options = lambda do |event|
+    params = event.payload[:params]
+               .except(*%w(controller action format))
+
     data = {
       time: Time.now,
       ip: event.payload[:ip],
@@ -13,12 +16,12 @@ Rails.application.configure do
       referer: event.payload[:referer],
       user_agent: event.payload[:user_agent],
       login_id: event.payload[:login_id],
+      params: params
     }
     if event.payload[:exception]
       data[:exception] = event.payload[:exception],
       data[:exception_object] = event.payload[:exception_object],
       data[:exception_backtrace] = event.payload[:exception_object].backtrace[0..6]
-
     end
 
     data
