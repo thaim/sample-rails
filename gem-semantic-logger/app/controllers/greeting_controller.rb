5abdbb9662a0280ac7e125b49475059c0b38fdd7
thaim <thaim24@gmail.com>
2020-08-22 06:07:34 +0000

    add sample logging

--- gem-semantic-logger/app/controllers/greeting_controller.rb
+++ gem-semantic-logger/app/controllers/greeting_controller.rb
@@ -1,6 +1,9 @@
 class GreetingController < ApplicationController
   def index
     logger.debug "greeting index"
+    logger.debug payload: {user: 'anonymous'}
+
+    logger.debug('hello message with payload', user: 'thaim')
 
     Rails.logger.info "hello message"
   end
