b8b132a50e2e4d92f0fab9fae907b8b3db4907e4
thaim <thaim24@gmail.com>
2020-08-22 12:23:53 +0000

    install sidekiq

--- gem-semantic-logger/config/routes.rb
+++ gem-semantic-logger/config/routes.rb
@@ -1,6 +1,8 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   get 'greeting/index'
-
+  mount Sidekiq::Web => '/sidekiq'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
